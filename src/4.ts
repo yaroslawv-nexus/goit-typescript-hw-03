effe873ad9b8bba544b98d5cc5e0f7c30da51b25
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-10-28 00:19:44 +0300

    finak task

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,65 @@
+
+
+
+
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature() {
+        return this.signature;
+    }
+
+}
+
+class Person {
+    constructor(private key: Key) { }
+    
+
+    getKey() {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door = false;
+    protected tenats: Person[] = [];
+    
+
+    constructor(protected key: Key) {}
+    
+   
+     comeIn(person: Person): void {
+        if (this.door) {
+            this.tenats.push(person);
+        } else {
+            console.log("Дверь закрыта");
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super(key);
+    }
+
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+            console.log("Дверь открыто");
+        }
+    }
+}
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
